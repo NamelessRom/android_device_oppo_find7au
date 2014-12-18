d4d7376b1e9215b7ad83ba44e05c5ee6d0cb338a
Evisceration <eviscerationls@gmail.com>
2014-12-18 16:58:51 +0100

    find7au: cleanup
    Change-Id: Iee276fe2c96e049b4af392995e259f1f23760ec5

--- nameless_find7au.mk
+++ nameless_find7au.mk
@@ -1,4 +1,30 @@
-$(call inherit-product, device/oppo/find7au/full_find7au.mk)
+# Copyright (C) 2014 The CyanogenMod Project
+# Copyright (C) 2014 The NamelessRom Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit from find7-common
+$(call inherit-product, device/oppo/find7-common/find7-common.mk)
+
+# overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+
+# Bootanimation and recovery
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH  := 1080
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Get the prebuilt list of APNs
 $(call inherit-product, vendor/nameless/config/apns.mk)
@@ -11,5 +37,18 @@ $(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
 
 PRODUCT_NAME := nameless_find7au
 PRODUCT_DEVICE := find7au
+PRODUCT_BRAND := OPPO
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_MODEL := Find7
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+# Device uses high-density artwork where available
+PRODUCT_AAPT_CONFIG := normal hdpi xhdpi xxhdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# call dalvik heap config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
+
+# call hwui memory config
+$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
